522324022aa29e796d977f8b9d31f89455a84b4f
Bao Tram <baotram1701.ct@gmail.com>
2023-04-05 00:10:12 +0700

    complete

--- main.js
+++ main.js
@@ -3,65 +3,77 @@ const body = document.querySelector('body')
 const numbers = document.querySelectorAll('.number')
 const clock = document.querySelector('.clock')
 
+const min = document.querySelector('.min')
+const hr = document.querySelector('.hr')
 
+let isNight = localStorage.getItem('isNight') || false
+let savedCurr = localStorage.getItem('currentTheme') || light.toString()
 
-let isNight = false
-
+let restoreTheme = eval( `(${savedCurr})` )
+restoreTheme()
 
 function themeToggle() {
-    const min = document.querySelector('.min')
-    const hr = document.querySelector('.hr')
     if (isNight) {
-
-        body.style.backgroundColor = 'white'
-        clock.style.backgroundImage = 'url("./imgs/light-mode.png") '
-
-        min.style.backgroundColor = 'black'
-        hr.style.backgroundColor = 'black'
-
-        numbers.forEach((number) => {
-            number.style.color = 'black'
-        })
-        frame.innerHTML = `
-        <div class="day">
-            <i class="fa-regular fa-sun"></i>
-        </div>
-        `
+        light()
+        savedCurr = localStorage.setItem('currentTheme', light.toString() )
     } else {
-        body.style.backgroundColor = 'black'
-        clock.style.backgroundImage = 'url("./imgs/dark-mode.png") '
-
-        min.style.backgroundColor = 'white'
-        hr.style.backgroundColor = 'white'
-
-        numbers.forEach((number) => {
-            number.style.color = 'white'
-        })
-        frame.innerHTML = `
-        <div class="night">
-            <i class="fa-regular fa-moon"></i>
-        </div>
-        `
+        dark()
+        savedCurr = localStorage.setItem('currentTheme', dark.toString() )
     }
     isNight = !isNight
+    localStorage.setItem('isNight', isNight)
 }
 
-const hr = document.querySelector('.hour')
-const min = document.querySelector('.minute')
-const sec = document.querySelector('.second')
+
+function light() {
+    body.style.backgroundColor = 'white'
+    clock.style.backgroundImage = 'url("./imgs/light-mode.png") '
+
+    min.style.backgroundColor = 'black'
+    hr.style.backgroundColor = 'black'
+
+    numbers.forEach((number) => {
+        number.style.color = 'black'
+    })
+    frame.innerHTML = `
+    <div class="day">
+        <i class="fa-regular fa-sun"></i>
+    </div>
+    `
+}
+
+function dark() { 
+    body.style.backgroundColor = 'black'
+    clock.style.backgroundImage = 'url("./imgs/dark-mode.png") '
+
+    min.style.backgroundColor = 'white'
+    hr.style.backgroundColor = 'white'
+
+    numbers.forEach((number) => {
+        number.style.color = 'white'
+    })
+    frame.innerHTML = `
+    <div class="night">
+        <i class="fa-regular fa-moon"></i>
+    </div>
+    `
+}
+
+const hour = document.querySelector('.hour')
+const minute = document.querySelector('.minute')
+const second = document.querySelector('.second')
 
 function time() {
     setInterval(() => {
         let day = new Date()
-        let hour = day.getHours() * 30
-        let minute = day.getMinutes() *6
-        let second = day.getSeconds() *6
+        let h = day.getHours() * 30
+        let m = day.getMinutes() *6
+        let s = day.getSeconds() *6
 
-        hr.style.transform = `rotate(${hour + minute/12}deg)`
-        min.style.transform = `rotate(${minute}deg)`
-        sec.style.transform = `rotate(${second}deg)`
+        hour.style.transform = `rotate(${h + m/12}deg)`
+        minute.style.transform = `rotate(${m}deg)`
+        second.style.transform = `rotate(${s}deg)`
     }) 
-
 }
 
 function start() {
